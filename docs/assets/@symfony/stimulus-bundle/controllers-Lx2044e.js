6a67cb236a32530e7f875e234cf745a60d7256aa
Ángel Cardiel Ferrero <angel.cardiel@protonmail.com>
2025-07-31 19:35:44 +0200

    build static website with stat

--- docs/assets/@symfony/stimulus-bundle/controllers-Lx2044e.js
+++
@@ -1,5 +0,0 @@
-import controller_0 from "../ux-chartjs/controller.js";
-import controller_1 from "../../controllers/hello_controller.js";
-export const eagerControllers = {"symfony--ux-chartjs--chart": controller_0, "hello": controller_1};
-export const lazyControllers = {"csrf-protection": () => import("../../controllers/csrf_protection_controller.js")};
-export const isApplicationDebug = true;
