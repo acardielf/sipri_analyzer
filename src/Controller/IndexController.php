7fb198b2fe8bd9b7bc7fdf44141e80abbde1e254
Ángel Cardiel Ferrero <angel.cardiel@protonmail.com>
2025-09-22 19:50:28 +0200

    info: new info in landing page.

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,18 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\ConvocatoriaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class IndexController extends AbstractController
 {
+    public function __construct(
+        protected readonly ConvocatoriaRepository $convocatoriaRepository,
+    ) {
+    }
 
     #[Route('/', name: 'app_index')]
     public function index(): Response
     {
-        return $this->render('index.html.twig');
+        return $this->render('index.html.twig', [
+            'convocatoria' => $this->convocatoriaRepository->findOneBy([], ['fecha' => 'DESC']),
+        ]);
     }
-
-
+    
 }
