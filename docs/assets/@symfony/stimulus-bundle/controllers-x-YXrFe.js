2220a3bf69ce3e356774e1a8852cbe07b5aaff0e
acardielf <1655777+acardielf@users.noreply.github.com>
2025-10-18 16:15:25 +0000

    Apply automatic changes

--- docs/assets/@symfony/stimulus-bundle/controllers-x-YXrFe.js
+++
@@ -1,8 +0,0 @@
-import controller_0 from "../ux-autocomplete/controller.js";
-import "tom-select/dist/css/tom-select.default.css";
-import controller_1 from "../ux-chartjs/controller.js";
-import controller_2 from "../../controllers/selector_provincias_controller.js";
-import controller_3 from "../../controllers/hello_controller.js";
-export const eagerControllers = {"symfony--ux-autocomplete--autocomplete": controller_0, "symfony--ux-chartjs--chart": controller_1, "selector-provincias": controller_2, "hello": controller_3};
-export const lazyControllers = {"csrf-protection": () => import("../../controllers/csrf_protection_controller.js")};
-export const isApplicationDebug = false;
